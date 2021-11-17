2102b236efdb52203fc1f751a8603d76d3313913
jasminejoy01 <55416635+jasminejoy01@users.noreply.github.com>
2021-11-17 01:06:22 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,32 @@
 import numpy as np
 
+def convertTuple(tup):
+    # initialize an empty string
+    str = ''
+    for item in tup:
+        str = str + item
+    return str
+
+def a(text):
+    chars = "\/*_{}[]()#+-!$';<>|:%=¸”&‚"
+    text = text.replace('"', " ")
+    text = text.replace(".", " ")
+    text = text.replace("\n", "")
+    text = text.replace("\t", " ")
+    for c in chars:
+        text = text.replace(c, " ")
+    text = text.replace("'", " ")
+    text = text.replace("filler", "'")
+    text = text.replace("  ", " ")
+    text = text.lower()
+    text = text.encode("ascii", "ignore")
+    text = text.decode()
+    return text
+
+def split(text):
+    s = text.split(" ")
+    return len(s)
+
 def create_matrix(tokens, padding):
     n = len(tokens)
     matrix = np.zeros(shape=(n,padding),dtype='object')
@@ -12,3 +39,4 @@ def create_matrix(tokens, padding):
         for j in range(0, len(value)):
             matrix[i][j] = int(value[j])
     return matrix
+
